a2674d511dbe69764fa251075f366ceae25b25b4
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2024-12-27 13:28:54 -0300

    feat: add user schema with properties

---
+++ src/user/schemas/user.schemas.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  nome: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: '12345' })
+  password: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
