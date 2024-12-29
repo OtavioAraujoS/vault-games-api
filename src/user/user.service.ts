fdf44bd4575ac4f8cddbfb5851f23a4ac0ebe99f
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2024-12-29 15:44:38 -0300

    feat: add loggs on functions

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   NotFoundException,
   BadRequestException,
+  Logger,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -9,17 +10,28 @@ import { User, UserDocument } from './schemas/user.schemas';
 import { LoginUserDto } from './dto/login-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
+import * as chalk from 'chalk';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async login(loginUserDto: LoginUserDto): Promise<User> {
     const { nome, password } = loginUserDto;
     const user = await this.userModel.findOne({ nome, password }).exec();
     if (!user) {
+      this.logger.error(
+        chalk.red(
+          `Login failed for user: ${nome} at ${new Date().toISOString()}`
+        )
+      );
       throw new NotFoundException('User not found or incorrect password');
     }
+    this.logger.log(
+      chalk.green(`User logged in: ${nome} at ${new Date().toISOString()}`)
+    );
     return user;
   }
 
@@ -28,9 +40,19 @@ export class UserService {
       .findOne({ nome: createUserDto.nome })
       .exec();
     if (existingUser) {
+      this.logger.error(
+        chalk.yellow(
+          `User creation failed: ${createUserDto.nome} already exists at ${new Date().toISOString()}`
+        )
+      );
       throw new BadRequestException('User already exists');
     }
     const createdUser = new this.userModel(createUserDto);
+    this.logger.log(
+      chalk.blue(
+        `User created: ${createUserDto.nome} at ${new Date().toISOString()}`
+      )
+    );
     return createdUser.save();
   }
 
@@ -46,8 +68,18 @@ export class UserService {
       )
       .exec();
     if (!user) {
+      this.logger.error(
+        chalk.hex('#FF00FF')(
+          `Password update failed for user ID: ${id} at ${new Date().toISOString()}`
+        )
+      );
       throw new NotFoundException('User not found');
     }
+    this.logger.log(
+      chalk.cyan(
+        `Password updated for user ID: ${id} at ${new Date().toISOString()}`
+      )
+    );
     return user;
   }
 }
