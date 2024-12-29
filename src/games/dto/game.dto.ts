f56fc4632c4d22642e64bdfa2bd322b43088df64
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2024-12-29 15:46:54 -0300

    feat: add GameDto for game data transfer object

---
+++ src/games/dto/game.dto.ts
@@ -0,0 +1,8 @@
+export class GameDto {
+  readonly id: number;
+  readonly user_id: number;
+  readonly nome: string;
+  readonly description: string;
+  readonly image: string;
+  readonly status: string;
+}
