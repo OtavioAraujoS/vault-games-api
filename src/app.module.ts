9a6433a327bcb9696a5a321ff39999899e6706ff
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-12-30 09:54:46 -0300

    feat: add GameModule to the main application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameModule } from './games/game.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -10,6 +11,7 @@ import { UserModule } from './user/user.module';
       'mongodb+srv://otavioaraujo490:BiiTJas8zsM9OONG@cluster0.gqgds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     ),
     UserModule,
+    GameModule,
   ],
   controllers: [AppController],
   providers: [AppService],
