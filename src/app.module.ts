4d730c3341e80475b60ab025b0369828ff1952aa
otavio.silva <otavioaraujo490@gmail.com>
2025-03-26 10:39:04 -0300

    feat: update MongoDB connection string to use environment variables for improved security

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DashboardModule } from './dashboard/dasboard.module';
 import { GameModule } from './games/game.module';
 import { UserModule } from './user/user.module';
-import { DashboardModule } from './dashboard/dasboard.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://otavioaraujo490:BiiTJas8zsM9OONG@cluster0.gqgds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+      `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0.gqgds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     ),
     UserModule,
     GameModule,
